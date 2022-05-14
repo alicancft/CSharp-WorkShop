d593b8834da858878680bc682b7f4cdc8a283948
alicancft <alicancft98@gmail.com>
2022-05-14 23:01:01 +0300

    add.SingleCouple

---
+++ SingleCouple/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SingleCouple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz = ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi % 2 == 0 )
+            {
+                Console.WriteLine("Girdiğiniz sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı tektir.");
+            }
+        }
+    }
+}
