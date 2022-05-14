5459adcc781b13d922b185b161169e0d0ce7c2fc
alicancft <alicancft98@gmail.com>
2022-05-14 19:00:07 +0300

    Update Program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace HelloWorld
+﻿
+namespace HelloWorld
 {
     internal class Program
     {
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(MyMethod(x, y));
         }
     }
 }
