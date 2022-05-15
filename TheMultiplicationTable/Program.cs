5d50a2769a062282c31ce4a591455c6bb1345922
alicancft <alicancft98@gmail.com>
2022-05-15 22:17:21 +0300

    Update: TheMultiplicationTable

--- TheMultiplicationTable/Program.cs
+++ TheMultiplicationTable/Program.cs
@@ -4,14 +4,24 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <=10; i++)
+            for (int i =0; i <= 10; i++)
             {
-                Console.WriteLine("---{0}'ler---",i);
-                for (int j = 1; j <=10; j++)
+                //Console.WriteLine("---{0}'ler---",i);
+                for (int j = 1; j <= 10; j++)
                 {
-                    Console.WriteLine("{0} * {1} = {2} ",i,j,i*j);
-                }
+                    if (i == 0)
+                    {
+                        Console.Write("---{0:d2}'ler--- ||", j);
+                    }
+                    else
+                    {
+                        Console.Write("{0:d2} * {1:d2} = {2:d2} ||", j, i, i * j);
+                    }
+                    //Console.WriteLine("{0} * {1} = {2} ",i,j,i*j);
 
+                    
+                }
+                Console.WriteLine();
             }
         }
     }
