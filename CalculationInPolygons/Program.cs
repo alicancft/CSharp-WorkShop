e5ea75fe442b829be6c74292e17e9479f767c12d
alicancft <alicancft98@gmail.com>
2022-05-15 21:49:25 +0300

    Add:VolumeOfSphere

--- CalculationInPolygons/Program.cs
+++ CalculationInPolygons/Program.cs
@@ -7,21 +7,34 @@
 
         }
 
-        public static double CircumferenceOfTheCircle(int radius)
+        public static float CircumferenceOfTheCircle(int radius)
         {
-            return 2 * Math.PI * radius;
+            return 2 * MathF.PI * radius;
         }
 
-        public static double AreaOfTheCircle(int radius)
+        public static float AreaOfTheCircle(int radius)
         {
-            return Math.PI * Math.Pow(radius, 2);
+            return MathF.PI * MathF.Pow(radius, 2);
         }
 
         public static int AreaOfRectangle(int a, int b)
         {
             return a * b;
         }
-        public static int 
 
+        public static int TheVolumeOfTheRectangle(int a, int b, int h)
+        {
+            return AreaOfRectangle(a, b) * h;
+        }
+
+        public static float AreaOfTheSphere(int radius)
+        {
+            return AreaOfTheCircle(radius) * 4;
+        }
+
+        public static float VolumeOfSphere(int radius)
+        {
+            return (4 / 3) * MathF.PI * MathF.Pow(radius, 3);
+        }
     }
 }
