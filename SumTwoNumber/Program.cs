39d1ce5df4b106338d39e80ffb24ae71017dcdda
alicancft <alicancft98@gmail.com>
2022-05-15 19:53:50 +0300

    Add.SumTwoNumbers

---
+++ SumTwoNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SumTwoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number = ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number = ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of numbers = " + (first+second));
+        }
+    }
+}
