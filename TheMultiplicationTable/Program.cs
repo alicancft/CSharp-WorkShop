a9e9aa80a6ac43413bf1caff50dd13accd26fa8e
alicancft <alicancft98@gmail.com>
2022-05-15 22:02:54 +0300

    Add:TheMultiplicationTable

---
+++ TheMultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TheMultiplicationTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine("---{0}'ler---",i);
+                for (int j = 1; j <=10; j++)
+                {
+                    Console.WriteLine("{0} * {1} = {2} ",i,j,i*j);
+                }
+
+            }
+        }
+    }
+}
