1932f4717e3d746cab2324883856fa9f960cbafa
alicancft <alicancft98@gmail.com>
2022-05-15 20:14:31 +0300

    Add.SumOfDigits

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SumOfDigits
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number = ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of Digits = "+SumOfDigit(number));
+        }
+
+        public static int SumOfDigit(int n)
+        {
+            int sum = 0;
+            while (n > 10)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            sum += n;
+            return sum;
+        }
+    }
+}
