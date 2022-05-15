f6fc19b4bae0f83be4de4e80bad3718e399e3024
alicancft <alicancft98@gmail.com>
2022-05-15 20:51:40 +0300

    Add.CalculationInPolygons

---
+++ CalculationInPolygons/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CalculationInPolygons
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static double CircumferenceOfTheCircle(int radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public static double AreaOfTheCircle(int radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static int AreaOfRectangle(int a, int b)
+        {
+            return a * b;
+        }
+        public static int 
+
+    }
+}
