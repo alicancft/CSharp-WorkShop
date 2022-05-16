ea0c9d3455919e84fdf36e096d87aeb7e958b3c7
alicancft <alicancft98@gmail.com>
2022-05-16 20:57:16 +0300

    Add:ArraySorting

---
+++ Array/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Array
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 4, 6, 8, 2, 3 };
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nMax " + array.Max());
+            Console.WriteLine("Min " + array.Min());
+            System.Array.Sort(array);
+
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
