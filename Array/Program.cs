ffd29f47d1133f08f945bec13c62373883be2b75
alicancft <alicancft98@gmail.com>
2022-05-16 21:30:08 +0300

    Add:StandardDeviation

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Array
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Array
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 4, 6, 8, 2, 3 };
+            int[] array = { 5, 2, 11, 12, 6, 3 };
             foreach (var item in array)
             {
                 Console.Write(item + " ");
@@ -12,12 +14,28 @@
 
             Console.WriteLine("\nMax " + array.Max());
             Console.WriteLine("Min " + array.Min());
+            var average = array.Average();
+            Console.Write(average);
             System.Array.Sort(array);
 
             foreach (var item in array)
             {
                 Console.Write(item + " ");
             }
+            Console.WriteLine("StandardDeviation = "+StandardDeviation(array));
+        }
+
+        public static double StandardDeviation(int[] n)
+        {
+            double standardDeviation=0;
+            double totalSquare = 0;
+            var average = n.Average();
+            for (int i = 0; i < n.Length; i++)
+            {
+                totalSquare += Math.Pow(n[i],2);
+            }
+            standardDeviation = ((1d / n.Length) * (totalSquare - (n.Length * Math.Pow(average, 2))));
+            return Math.Sqrt(standardDeviation);
         }
     }
 }
