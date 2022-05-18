b0b431829b42bd8a81c1b1523311339c36e71bcf
alicancft <alicancft98@gmail.com>
2022-05-18 22:07:45 +0300

    Add:Pattern

---
+++ Pattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Pattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+    }
+}
