b9e484118185ff987719f0d37df0f0a584246b88
alicancft <alicancft98@gmail.com>
2022-05-19 00:27:09 +0300

    Add.Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Factorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} sayısının faktöriyeli = {1} ",number,Factorial(number));
+        }
+
+        public static int Factorial(int number)
+        {
+            int fac = 1;
+            for (int i = number; i > 0; i--)
+            {
+                fac *= i;
+            }
+            return fac;
+        }
+    }
+}
