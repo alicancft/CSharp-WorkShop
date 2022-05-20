d0945a32306192be896c15c80874706ba109e767
alicancft <alicancft98@gmail.com>
2022-05-20 23:40:12 +0300

    Update.BinaryToDecimal

--- BinaryToDesimal/Program.cs
+++ BinaryToDesimal/Program.cs
@@ -7,17 +7,11 @@
             string ikilikSayi = Console.ReadLine();
             int n = ikilikSayi.Length;
             int toplam = 0;
-            string basamaklar = "";
             for (int i = 0; i < n; i++)
             {
-                basamaklar+= ikilikSayi[n - i - 1];
-            }
-            for (int i = 0; i < n; i++)
-            {
-                toplam += Convert.ToInt32(basamaklar[i].ToString())*(int)(Math.Pow(2,i));
+                toplam += Convert.ToInt32(ikilikSayi[n - i - 1].ToString()) * (int)(Math.Pow(2, i));
             }
             Console.WriteLine(toplam);
-            
         }
     }
 }
