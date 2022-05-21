c19ecb59d56e104b8bc7af2a6e48febdb446ca5d
alicancft <alicancft98@gmail.com>
2022-05-21 18:14:52 +0300

    Add:MatrixCreate And MatrixWrite

---
+++ Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Matrix
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            string[,] matrix = MatrisOlustur(i,j); 
+            MatrisYazdir(matrix);
+        }
+        public static string[,] MatrisOlustur(int a,int b)
+        {
+            string[,] matris= new string[a,b];
+            for (int k = 0; k < a; k++)
+            {
+                for (int l = 0; l < b; l++)
+                {
+                    matris[k, l] = k + "," + l;
+                }
+            }
+            return matris;
+        }
+        public static void MatrisYazdir(string[,] matriss)
+        {
+            for (int i = 0; i < matriss.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriss.GetLength(1); j++)
+                {
+                    Console.Write(matriss[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void MatrisYazdir(int[,] matriss)
+        {
+            for (int i = 0; i < matriss.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriss.GetLength(1); j++)
+                {
+                    Console.Write(matriss[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
