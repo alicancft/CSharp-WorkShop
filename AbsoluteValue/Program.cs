d54eaaf643a84da8c33708479eede7a6156989ab
alicancft <alicancft98@gmail.com>
2022-06-04 19:52:28 +0300

    Add.Collections

--- AbsoluteValue/Program.cs
+++ AbsoluteValue/Program.cs
@@ -2,7 +2,7 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             Console.Write("Enter a number = ");
             int x = Convert.ToInt32(Console.ReadLine());
---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList cities = new ArrayList();
+            cities.Add("Ankara");
+            cities.Add("Adana");
+            cities.Add("İzmir");
+            cities.Add(1);
+            foreach (var VARIABLE in cities)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
